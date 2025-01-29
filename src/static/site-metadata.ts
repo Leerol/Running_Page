73d13aa35f65793fea9f96b43cec34d790f91769
Leerol <59154783+Leerol@users.noreply.github.com>
2025-01-30 02:59:00 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'LI RUI Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://running-page-phi-lilac.vercel.app/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://running-page-phi-lilac.vercel.app/',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://running-page-phi-lilac.vercel.app/',
     },
   ],
 };
